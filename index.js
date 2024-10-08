33c4e7ca952afb75e218c59c5806755f01f9475d
David <dd.gomes202@gmail.com>
2024-10-08 01:05:10 -0300

    multi chate sep. de sala

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-const express = require('express')
-const socketIo = require('socket.io')
-const http = require('http')
-const path = require('path')
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server)
+const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-io.on('connection', (socket) =>{
-    console.log('Novo usuário conectado!')
 
-    socket.on('chat message', (data) =>{
-        console.log(data)
-        io.emit('chat message', {user: data.user, message: data.message, time: data.time});
-        
-    })
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado!');
+
+    socket.on('joinRoom', (room) => {
+        socket.join(room); 
+        console.log(`Usuário entrou na sala: ${room}`);
+    });
+    
+    socket.on('chat message', (data) => {
+        console.log(`Mensagem recebida na sala ${data.room}: ${data.message}`);
+       
+        io.to(data.room).emit('chat message', { user: data.user, message: data.message, time: data.time, room: data.room });
+    });
 
     socket.on('disconnect', () => {
         console.log('Usuário desconectado');
-    })
-})
+    });
+});
 
-server.listen(3000, () =>{
-    console.log('Servidor rodando na porta 3000')
-})
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
